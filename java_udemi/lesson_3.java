a71118023430890a7bfbb9d4776cb871cc88bd38
CristianE-C <cristian.enicarp@mail.ru>
2023-10-31 12:50:08 +0200

    adding the solution for lesson 3

--- java_udemi/lesson_3.java
+++ java_udemi/lesson_3.java
@@ -2,7 +2,30 @@ package java_udemi;
 public class lesson_3 {
     public static void main(String [] args) {
 
-        float a=9.0F;
+        // ex1
+        int i1 = 5;
+        int i2 = 11;
+        double d1 = 5.5;
+        double d2 = 1.3;
+        long l = 20L;
+        double result = 0;
+        result = i2 / d1 + d2 % i1 - l;
+        
+        System.out.println(result);
+
+        // ex2
+        int a = 5;
+        int b = 8;
+
+        System.out.println(a-- - --a + ++a + a++ + a);
+        //                 5   -   3 +   4 + 4   + 5 = 15
+        System.out.println(a);
+        //          a = 5
+
+        System.out.println(++b - b++ + ++b - --b);
+        //                   9 - 9   + 11  -   10 = 1
+        System.out.println(b);
+        //          b = 10
 
     }
     
